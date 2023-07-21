c7e1db0c674e88ad83283ae162cef6c9cb933d80
Jason Wright <jason.wright90@gmail.com>
2023-07-21 19:38:25 +0100

    test vercel

--- api/server.js
+++ api/server.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.post('/dream', async(req, res) => {
+app.post('/generate', async(req, res) => {
 
     try {
         const prompt = req.body.prompt;
--- main.js
+++ main.js
@@ -8,7 +8,7 @@ form.addEventListener('submit', async (e) => {
 
     const data = new FormData(form);
 
-    const response = await fetch('http://localhost:8080/dream', {
+    const response = await fetch('/api/server', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
